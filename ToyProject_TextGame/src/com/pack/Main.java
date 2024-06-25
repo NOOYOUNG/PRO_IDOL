b0a8adbacef5c273b4b7dfb9776a6bf1690f1ce7
2425343021 <chyooy23@gmail.com>
2024-06-25 18:31:33 +0900

    .

---
+++ ToyProject_TextGame/src/com/pack/Main.java
@@ -0,0 +1,11 @@
+package com.pack;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		GameController GC=new GameController();
+		GC.intro();
+	}
+
+}
